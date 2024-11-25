38037fdbf54896ad89d625cf16753f00bb184bb2
Haider Mukhtar <haidermukhtarch405@gmail.com>
2024-11-25 18:50:21 +0500

    Stylish v0.0.5
    Add App Intro Slider

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React, { useEffect } from 'react'
-import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
+import { Image, StyleSheet, Text, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
+
 //Splash Screen
 import SplashScreen from 'react-native-splash-screen'
-//My App Screens
-import SignIn from './screens/SignIn'
+
+import AppIntroSlider from 'react-native-app-intro-slider';
+
+// Intro Slider
+const slides = [
+  {
+    key: 1,
+    page: 1,
+    title: 'Choose Products',
+    text: 'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.',
+    image: require('./assets/onboarding_images/onboarding_1.png'),
+    backgroundColor: '#ffffff',
+  },
+  {
+    key: 2,
+    page: 2,
+    title: 'Make Payment',
+    text: 'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.',
+    image: require('./assets/onboarding_images/onboarding_2.png'),
+    backgroundColor: '#ffffff',
+  },
+  {
+    key: 3,
+    page: 3,
+    title: 'Get Your Order',
+    text: 'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.',
+    image: require('./assets/onboarding_images/onboarding_3.png'),
+    backgroundColor: '#ffffff',
+  }
+];
 
 const App = () => {
 
@@ -14,15 +41,72 @@ const App = () => {
     SplashScreen.hide();
   }, []);
 
-  //Stack Navigator
-  const Stack = createStackNavigator();
+  const [showHomePage, setShowHomePage] = useState(false);
+
+  const buttonLabel = (label) => {
+    return (
+      <View style={{ marginTop: 10 }}>
+        <Text style={{ color: "#F83758", fontSize: 18, fontWeight: '600', fontFamily: 'Montserrat-Bold' }}>
+          {label}
+        </Text>
+      </View>
+    )
+  }
+
+  if (!showHomePage) {
+    return (
+      <AppIntroSlider
+        data={slides}
+        renderItem={({ item }) => {
+          return (
+            <View style={{ flex: 1, }}>
+              <View style={{ flexDirection: 'row', alignItems: 'flex-start', marginHorizontal: 17, marginTop: 17, }}>
+                <Text style={{ fontSize: 18, fontWeight: '800', fontFamily: 'Montserrat-ExtraBold', color: '#000000' }}>
+                  {item.page}
+                </Text>
+                <Text style={{ fontSize: 18, fontWeight: '800', fontFamily: 'Montserrat-ExtraBold', color: '#A0A0A1' }}>
+                  /3
+                </Text>
+              </View>
+              <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', }}>
+                <Image
+                  source={item.image}
+                  style={{ height: 300, resizeMode: 'contain' }}
+                />
+                <Text style={{ fontSize: 24, fontWeight: '800', fontFamily: 'Montserrat-ExtraBold', textAlign: 'center', }}>
+                  {item.title}
+                </Text>
+                <Text style={{ fontSize: 14, fontWeight: '600', fontFamily: 'Montserrat-Regular', marginHorizontal: 50, textAlign: 'center', marginTop: 15, }}>
+                  {item.text}
+                </Text>
+              </View>
+            </View>
+          )
+        }}
+        activeDotStyle={{
+          backgroundColor: '#17223B',
+          width: 40,
+        }}
+        showPrevButton
+        showNextButton
+        showSkipButton
+        renderSkipButton={() => buttonLabel("Skip")}
+        renderNextButton={() => buttonLabel("Next")}
+        renderPrevButton={() => buttonLabel("Prev")}
+        renderDoneButton={() => buttonLabel("Done")}
+        onDone={() => {
+          setShowHomePage(true)
+        }}
+      />
+    )
+  }
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="SignIn" component={SignIn} options={{ headerShown: false }} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View>
+      <Text>
+        Home Screen
+      </Text>
+    </View>
   )
 }
 
---
+++ src/router/router.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const router = () => {
+  return (
+    <View>
+      <Text>router</Text>
+    </View>
+  )
+}
+
+export default router
+
+const styles = StyleSheet.create({})
